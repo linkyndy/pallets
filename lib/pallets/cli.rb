90c00d345e4e9eb630ef234923e1371ec701ea38
Andrei Horak <linkyndy@gmail.com>
2018-03-23 14:03:15 +0100

    Add option parsing to CLI app

--- lib/pallets/cli.rb
+++ lib/pallets/cli.rb
@@ -1,9 +1,12 @@
+require 'optparse'
+
 module Pallets
   class CLI
     def initialize
       @manager = Manager.new
       @signal_reader, @signal_writer = IO.pipe
 
+      parse_options
       setup_signal_handlers
     end
 
@@ -32,6 +35,38 @@ module Pallets
       when 'INT'
         raise Interrupt
       end
+    end
+
+    def parse_options
+      # TODO: -v for verbose, -d for debug, -q for quiet, -V for version
+      OptionParser.new do |opts|
+        opts.banner = 'Usage: pallets [options]'
+
+        opts.on('-b', '--backend BACKEND', 'Backend to use') do |backend|
+          Pallets.configuration.backend = backend
+        end
+
+        opts.on('-n', '--namespace NAMESPACE', 'Namespace to use for backend') do |namespace|
+          Pallets.configuration.namespace = namespace
+        end
+
+        opts.on('-p', '--pool NUM', Integer, 'Size of backend pool') do |pool|
+          Pallets.configuration.pool_size = pool
+        end
+
+        opts.on('-s', '--serializer SERIALIZER', 'Serializer to use') do |serializer|
+          Pallets.configuration.serializer = serializer
+        end
+
+        opts.on('-u', '--blocking-timeout NUM', Integer, 'Seconds to wait for new work') do |blocking_timeout|
+          Pallets.configuration.blocking_timeout = blocking_timeout
+        end
+
+        opts.on_tail('-h', '--help', 'Show this message') do
+          puts opts
+          exit
+        end
+      end.parse!
     end
 
     def setup_signal_handlers
