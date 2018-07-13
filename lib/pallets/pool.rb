1b0d74bb6867a77a5afbfd4f9c1a2f2fb8b26b05
Andrei Horak <linkyndy@gmail.com>
2018-07-13 15:07:25 +0200

    Add pool specs

--- lib/pallets/pool.rb
+++ lib/pallets/pool.rb
@@ -1,21 +1,23 @@
 module Pallets
   class Pool
-    def initialize(size:)
-      @queue = SizedQueue.new(size)
+    def initialize(size)
+      @queue = Queue.new
+      @size = size
       size.times { @queue << yield }
-      Pallets.logger.info "[pool] Initialized pool"
+    end
+
+    def size
+      @queue.size
     end
 
     def execute
-      raise unless block_given?
+      raise ArgumentError, 'Pool needs a block to execute' unless block_given?
 
       begin
         item = @queue.pop
-        Pallets.logger.info "[pool] Item popped..."
         yield item
       ensure
         @queue << item
-        Pallets.logger.info "[pool] ...Item pushed"
       end
     end
   end
---
+++ spec/pool_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Pallets::Pool do
+  it 'initializes the correct number of items' do
+    pool = Pallets::Pool.new(10) { :foo }
+    expect(pool.size).to eq(10)
+  end
+
+  describe '#execute' do
+    subject { Pallets::Pool.new(1) { :foo } }
+
+    it 'raises an error if no block is provided' do
+      expect { subject.execute }.to raise_error(ArgumentError)
+    end
+
+    it 'yields an item to the provided block' do
+      expect do |b|
+        subject.execute(&b)
+      end.to yield_with_args(:foo)
+    end
+
+    it 'puts back the item' do
+      subject.execute { }
+      expect(subject.size).to eq(1)
+    end
+  end
+end
