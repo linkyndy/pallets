e3d68e24d216331a3c85b795874a9356221da152
Andrei Horak <linkyndy@gmail.com>
2018-09-30 15:45:18 +0200

    Improve JSON serializer tests

--- spec/serializers/json_spec.rb
+++ spec/serializers/json_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 describe Pallets::Serializers::Json do
   let(:json_class) { class_double('JSON').as_stubbed_const }
 
-  before do
-    allow(json_class).to receive(:generate)
-    allow(json_class).to receive(:parse)
+  it 'properly serializes and deserializes data' do
+    expect(subject.load(subject.dump('foo'))).to eq('foo')
   end
 
   describe '#dump' do
+    before do
+      allow(json_class).to receive(:generate)
+    end
+
     it 'generates JSON' do
       subject.dump('foo')
       expect(json_class).to have_received(:generate).with('foo')
@@ -16,7 +19,11 @@ describe Pallets::Serializers::Json do
   end
 
   describe '#load' do
-    it 'generates JSON' do
+    before do
+      allow(json_class).to receive(:parse)
+    end
+
+    it 'parses JSON' do
       subject.load('foo')
       expect(json_class).to have_received(:parse).with('foo')
     end
