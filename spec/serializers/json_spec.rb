fc1f5758d7d741f0aa5eab70d327aba344fbacc4
Andrei Horak <linkyndy@gmail.com>
2019-02-11 17:52:01 +0100

    Define serializer methods for contexts

--- spec/serializers/json_spec.rb
+++ spec/serializers/json_spec.rb
@@ -7,6 +7,10 @@ describe Pallets::Serializers::Json do
     expect(subject.load(subject.dump('foo' => 'bar'))).to eq('foo' => 'bar')
   end
 
+  it 'properly serializes and deserializes context data' do
+    expect(subject.load_context(subject.dump_context('foo' => 'bar'))).to eq('foo' => 'bar')
+  end
+
   describe '#dump' do
     before do
       allow(json_class).to receive(:generate)
@@ -26,6 +30,28 @@ describe Pallets::Serializers::Json do
     it 'parses JSON' do
       subject.load('foo')
       expect(json_class).to have_received(:parse).with('foo')
+    end
+  end
+
+  describe '#dump_context' do
+    before do
+      allow(json_class).to receive(:generate)
+    end
+
+    it 'generates JSON for hash values' do
+      subject.dump_context('foo' => 'bar')
+      expect(json_class).to have_received(:generate).with('bar')
+    end
+  end
+
+  describe '#load' do
+    before do
+      allow(json_class).to receive(:parse)
+    end
+
+    it 'parses JSON for hash values' do
+      subject.load_context('foo' => 'bar')
+      expect(json_class).to have_received(:parse).with('bar')
     end
   end
 end
--- spec/serializers/msgpack_spec.rb
+++ spec/serializers/msgpack_spec.rb
@@ -7,6 +7,10 @@ describe Pallets::Serializers::Msgpack do
     expect(subject.load(subject.dump('foo'))).to eq('foo')
   end
 
+  it 'properly serializes and deserializes context data' do
+    expect(subject.load_context(subject.dump_context('foo' => 'bar'))).to eq('foo' => 'bar')
+  end
+
   describe '#dump' do
     before do
       allow(msgpack_class).to receive(:pack)
@@ -26,6 +30,28 @@ describe Pallets::Serializers::Msgpack do
     it 'unpacks data' do
       subject.load('foo')
       expect(msgpack_class).to have_received(:unpack).with('foo')
+    end
+  end
+
+  describe '#dump_context' do
+    before do
+      allow(msgpack_class).to receive(:pack)
+    end
+
+    it 'packs hash values' do
+      subject.dump_context('foo' => 'bar')
+      expect(msgpack_class).to have_received(:pack).with('bar')
+    end
+  end
+
+  describe '#load_context' do
+    before do
+      allow(msgpack_class).to receive(:unpack)
+    end
+
+    it 'unpacks hash values' do
+      subject.load_context('foo' => 'bar')
+      expect(msgpack_class).to have_received(:unpack).with('bar')
     end
   end
 end
