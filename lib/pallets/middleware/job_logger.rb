127739541bd09019917f2a54ebdda67700917428
Andrei Horak <linkyndy@gmail.com>
2019-05-03 10:43:56 +0200

    Return the result of the block

--- lib/pallets/middleware/job_logger.rb
+++ lib/pallets/middleware/job_logger.rb
@@ -3,8 +3,9 @@ module Pallets
     class JobLogger
       def self.call(worker, job, context)
         Pallets.logger.info 'Started', extract_metadata(worker.id, job)
-        yield
+        result = yield
         Pallets.logger.info 'Done', extract_metadata(worker.id, job)
+        result
       rescue => ex
         Pallets.logger.warn "#{ex.class.name}: #{ex.message}", extract_metadata(worker.id, job)
         Pallets.logger.warn ex.backtrace.join("\n"), extract_metadata(worker.id, job) unless ex.backtrace.nil?
