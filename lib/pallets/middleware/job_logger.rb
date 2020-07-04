60daa72e331d31c89ec48b87857b2a86433fa980
Andrei Horak <linkyndy@gmail.com>
2020-07-04 23:27:07 +0200

    Add job duration

--- lib/pallets/middleware/job_logger.rb
+++ lib/pallets/middleware/job_logger.rb
@@ -2,13 +2,15 @@ module Pallets
   module Middleware
     class JobLogger
       def self.call(worker, job, context)
+        start_time = current_time
+
         Pallets.logger.with_metadata(extract_metadata(worker.id, job)) do
           Pallets.logger.info 'Started'
           result = yield
-          Pallets.logger.info 'Done'
+          Pallets.logger.info "Done in #{(current_time - start_time).round(3)}s"
           result
         rescue => ex
-          Pallets.logger.warn 'Failed'
+          Pallets.logger.warn "Failed after #{(current_time - start_time).round(3)}s"
           Pallets.logger.warn "#{ex.class.name}: #{ex.message}"
           Pallets.logger.warn ex.backtrace.join("\n") unless ex.backtrace.nil?
           raise
@@ -23,6 +25,10 @@ module Pallets
           wf:   job['workflow_class'],
           tsk:  job['task_class'],
         }
+      end
+
+      def self.current_time
+        Process.clock_gettime(Process::CLOCK_MONOTONIC)
       end
     end
   end
